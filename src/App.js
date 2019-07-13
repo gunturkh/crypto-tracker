086fb2b6979553ecd3ffa53fdf2c4d6044348ee0
guntur <gunturkh@gmail.com>
2019-07-13 13:24:20 +0700

    Edit Name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Crypto Tracker
         </a>
       </header>
     </div>
